f71b931ead0f07b6827d6aa7c0f1f1e9fbd3bc02
Rocky Haque <rockyhaque99@gmail.com>
2024-06-24 18:55:21 +0600

    database set done

--- index.js
+++ index.js
@@ -1,6 +1,67 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+// config
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// database connection
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@revive.2tkcldw.mongodb.net/?retryWrites=true&w=majority&appName=Revive`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("craftDB");
+
+    const craftCollection = database.collection("crafts")
+
+    // craft apis
+    app.post("/craft", async(req, res) => {
+        const newCraft = req.body;
+        const result = await craftCollection.insertOne(newCraft);
+        console.log(result);
+        res.send(result);
+    })
+
+    app.get("/craft/:email", async(req, res) => {
+        const email = req.params.email;
+        const query = {email: email};
+        const result = await craftCollection.find(query).toArray();
+        console.log(result);
+        res.send(result)
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send("Hello world");
@@ -9,3 +70,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
+
